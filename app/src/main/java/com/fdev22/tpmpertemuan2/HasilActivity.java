a1ae385937e184e3840f3bae749eca3f49933e31
fsatriaa <47685262+fsatriaa@users.noreply.github.com>
2020-02-17 01:34:44 +0700

    Initial commit

---
+++ app/src/main/java/com/fdev22/tpmpertemuan2/HasilActivity.java
@@ -0,0 +1,24 @@
+package com.fdev22.tpmpertemuan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HasilActivity extends AppCompatActivity {
+    private TextView tvHasil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil);
+
+        tvHasil = findViewById(R.id.tv_hasil);
+
+        Double hasil = getIntent().getDoubleExtra("hasil", 0);
+
+        String tHasil = ""+hasil;
+
+        tvHasil.setText(tHasil);
+    }
+}
